108fdd94447299ce7d41db213020d77241588d22
Aneesh Srivastava <srivastavaaneesh004@gmail.com>
2023-05-03 22:01:49 +0530

    Day-1/home, about and blog page added

---
+++ ciblog/application/views/templates/header.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>ciBlog</title>
+<!-- /Applications/XAMPP/xamppfiles/htdocs/ciblog/application/views/templates/bootstrap.min.css -->
+<!-- application/assets/css/style.css -->
+<link href="<?php echo base_url(); ?>views/templates/bootstrap.min.css" rel="stylesheet" type="text/css" >
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/">ciBlog</a>
+        </div>
+        <div id="navbar">
+            <ul class="nav navbar-nav">
+                <li><a href="<?php echo base_url(); ?>">Home</a></li>
+                <li><a href="<?php echo base_url(); ?>about">About</a></li>
+                <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container">
+
